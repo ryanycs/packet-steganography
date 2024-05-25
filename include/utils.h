cdb079d87aaf17fba1c7a7e59734c47da1b16b86
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-05-25 19:13:10 +0800

    Feat: Add read_pcap function to utils.h and read.c

---
+++ include/utils.h
@@ -0,0 +1,11 @@
+#include <pcap.h>
+
+/// @brief
+/// @param filename pcap file name
+/// @param protocal protocol type
+/// @param start start index
+/// @param size  read size
+/// @param data return data
+/// @param data_size return data size
+/// @return 0 if success, -1 if failed
+int read_pcap(const char *filename, int protocal, int start, int size, char **data, int *data_size);
---
+++ src/read.c
@@ -0,0 +1,4 @@
+#include <utils.h>
+
+int read_pcap(const char *filename, int protocal, int start, int size, char **data, int *data_size) {
+}
