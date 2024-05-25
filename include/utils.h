7eede9511c9a0211a2c1337fada880c7e3203053
Ryan.Y.C.Su <Ryan.Y.C.Su@gmail.com>
2024-05-25 20:02:43 +0800

    Feat: Add protocol enum in utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,12 @@
 #include <pcap.h>
 
+enum Protocal {
+    IP,
+    TCP,
+    UDP,
+    RTP,
+};
+
 /// @brief
 /// @param filename pcap file name
 /// @param protocal protocol type
@@ -7,5 +14,5 @@
 /// @param size  read size
 /// @param data return data
 /// @param data_size return data size
-/// @return 0 if success, -1 if failed
+/// @return 0 if success, 1 if failed
 int read_pcap(const char *filename, int protocal, int start, int size, char **data, int *data_size);
